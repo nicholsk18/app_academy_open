8a056dac878b05a50d90c6ccea6360d2af219a9b
Karson Nichols <nicholsk18@gmail.com>
2020-11-11 12:31:52 -0600

    added rspec part 1 exercise

---
+++ software_engineering_foundations/rspec_exercise_2/lib/part_1.rb
@@ -0,0 +1,67 @@
+def partition(arr, num)
+  sub_1 = []
+  sub_2 = []
+
+  arr.each do |ele|
+    if ele < num
+      sub_1 << ele
+    else
+      sub_2 << ele
+    end
+  end
+
+  [sub_1, sub_2]
+end
+
+
+def merge(hash_1, hash_2)
+  main_hash = Hash.new(0)
+  hash_1.each { |key, value| main_hash[key] = value }
+  hash_2.each { |key, value| main_hash[key] = value }
+
+  main_hash
+end
+
+
+def censor(str, arr)
+  parts = []
+
+  str.split.each do |word|
+    if arr.include?(word.downcase)
+      parts << censor_vowel(word)
+    else
+      parts << word
+    end
+  end
+
+  parts.join(" ")
+end
+
+def censor_vowel(word)
+  vowels = "aeiouAEIOU"
+  new_word = ""
+
+  word.each_char do |char|
+    if vowels.include?(char)
+      new_word += "*"
+    else
+      new_word += char
+    end
+  end
+
+  new_word
+end
+
+def power_of_two?(num)
+  return true if num == 1
+
+  (1...num).each do |n|
+    if 2.pow(n) == num
+      return true
+    end
+  end
+
+  return false
+end
+
+p 2.pow(1)
