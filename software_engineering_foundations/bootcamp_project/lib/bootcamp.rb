61891286396212e02840c8b4857cb6b212804446
Karson Nichols <nicholsk18@gmail.com>
2020-11-13 13:16:52 -0600

    finished part one of bootcamp project

--- software_engineering_foundations/bootcamp_project/lib/bootcamp.rb
+++ software_engineering_foundations/bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,48 @@
 class Bootcamp
-  
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new { |hash, key| hash[key] = [] }
+  end
+
+  def name
+    @name
+  end
+
+  def slogan
+    @slogan
+  end
+
+  def teachers
+    @teachers
+  end
+
+  def students
+    @students
+  end
+
+  def hire(teacher)
+    @teachers << teacher
+  end
+
+  def enroll(student)
+    if @students.length < @student_capacity
+      @students << student
+      return true
+    end
+
+    false
+  end
+
+  def enrolled?(student)
+    if @students.include?(student)
+      return true
+    end
+
+    false
+  end
+
 end
