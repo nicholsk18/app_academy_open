e535647bd1507c25352df02aeeee2b903837b434
Karson <nicholsk18@gmail.com>
2020-11-14 10:26:31 -0600

    finished working on part one of hangman game. Started to work on part two

---
+++ software_engineering_foundations/hangman_project/lib/hangman.rb
@@ -0,0 +1,60 @@
+class Hangman
+  DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+  
+  def self.random_word
+    DICTIONARY.sample
+  end
+
+  def initialize
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length, "_")
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
+
+  def guess_word
+    @guess_word
+  end
+
+  def attempted_chars
+    @attempted_chars
+  end
+
+  def remaining_incorrect_guesses
+    @remaining_incorrect_guesses
+  end
+
+  def already_attempted?(char)
+    @attempted_chars.include?(char)
+  end
+
+  def get_matching_indices(char)
+    indices = []
+
+    @secret_word.split("").each.with_index do |ele, index|
+      if ele == char
+        indices << index
+      end
+    end
+
+    indices
+  end
+
+  def fill_indices(char, array)
+    array.each { |index| @guess_word[index] = char }
+  end
+
+  def try_guess(char)
+    if self.already_attempted?(char)
+      p "that has already been attempted"
+      return false
+    end
+
+    # @attempted_chars << char
+    # if !(@secret_word.split("").include(char))
+    #   @remaining_incorrect_guesses -= 1 
+    # end
+
+    # true
+  end
+end
