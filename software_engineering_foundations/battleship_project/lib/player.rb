8276596d80f364ef4def8f24a51186e07cc2949c
Karson Nichols <nicholsk18@gmail.com>
2020-11-18 08:38:35 -0600

    added player

--- software_engineering_foundations/battleship_project/lib/player.rb
+++ software_engineering_foundations/battleship_project/lib/player.rb
@@ -1,3 +1,7 @@
 class Player
-
+  def get_move
+    puts "enter a position with coordinates separated with a space like '4 7'"
+    user_points = gets.chomp.split.map { |num| num.to_i }
+    user_points
+  end
 end
