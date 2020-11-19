2ec5aaea092b2079f642fa4ee6975c0c8dd781e0
Karson <nicholsk18@gmail.com>
2020-11-18 21:27:29 -0600

    converted to ruby shorthand for map

--- software_engineering_foundations/battleship_project/lib/player.rb
+++ software_engineering_foundations/battleship_project/lib/player.rb
@@ -1,7 +1,6 @@
 class Player
   def get_move
     puts "enter a position with coordinates separated with a space like '4 7'"
-    user_points = gets.chomp.split.map { |num| num.to_i }
-    user_points
+    gets.chomp.split.map(&:to_i)
   end
 end
