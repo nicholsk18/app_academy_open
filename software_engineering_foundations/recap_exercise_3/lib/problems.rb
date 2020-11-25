4d8f0a0e7c8cbeb4d6fb609a7554f27f2f825de6
Karson Nichols <nicholsk18@gmail.com>
2020-11-25 13:40:11 -0600

    added exercise 3

---
+++ software_engineering_foundations/recap_exercise_3/lib/problems.rb
@@ -0,0 +1,51 @@
+
+def no_dupes?
+
+end
+
+def no_consecutive_repeats?(arr)
+
+end
+
+def char_indices(str)
+
+end
+
+def longest_streak(str)
+
+end
+
+def bi_prime?(num)
+
+end
+
+def vigenere_cipher(message, key)
+
+end
+
+def vowel_rotate(str)
+
+end
+
+class String
+  def select(&prc)
+
+  end
+
+  def map!(&prc)
+
+  end
+end
+
+def multiply(a, b)
+
+end
+
+def lucas_sequence(num)
+
+end
+
+def prime_factorization(num)
+
+end
+
