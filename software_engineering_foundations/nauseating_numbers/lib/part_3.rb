082bc3ae83971c561fd8c9e0ecc5f783024ba803
Karson Nichols <nicholsk18@gmail.com>
2020-12-11 08:23:13 -0600

    finished reload

--- software_engineering_foundations/nauseating_numbers/lib/part_3.rb
+++ software_engineering_foundations/nauseating_numbers/lib/part_3.rb
@@ -1,5 +1,13 @@
 def matrix_addition_reloaded(*matrices)
+  matrix = matrices.first
+  height = matrix.length
+  width = matrix[0].length
 
+  empty_matrix = Array.new(height) { [0] * width }
+  matrices.inject(empty_matrix) do |m1, m2|
+    return nil if m2.length != height or m2[0].length != width
+    matrix_addition(m1, m2)
+  end
 end
 
 def squarocol?(array)
