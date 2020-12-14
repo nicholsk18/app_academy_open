00df105651d7a680cb3a703a827fb4aed113fabc
Karson Nichols <nicholsk18@gmail.com>
2020-12-14 13:16:19 -0600

    mersenne prime

--- software_engineering_foundations/nauseating_numbers/lib/part_4.rb
+++ software_engineering_foundations/nauseating_numbers/lib/part_4.rb
@@ -1,5 +1,18 @@
-def mersenne_prime(number)
+def is_prime?(num)
+  return  false if num < 2
+  (2...num).none? { |i| num % i == 0 }
+end
 
+def mersenne_prime(number)
+  exp = -1
+  count = 0
+
+  while count < number
+    exp += 1
+    mersenne = 2 ** exp - 1
+    count += 1 if is_prime?(mersenne)
+  end
+  2 ** exp - 1
 end
 
 def triangular_word?(word)
