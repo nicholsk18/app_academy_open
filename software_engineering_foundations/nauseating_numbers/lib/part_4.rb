fcd01248a0c9d6419ffe843e7868ab0473a34ac0
Karson Nichols <nicholsk18@gmail.com>
2020-12-15 08:50:41 -0600

    finished nauseating numbers

--- software_engineering_foundations/nauseating_numbers/lib/part_4.rb
+++ software_engineering_foundations/nauseating_numbers/lib/part_4.rb
@@ -15,14 +15,88 @@ def mersenne_prime(number)
   2 ** exp - 1
 end
 
-def triangular_word?(word)
+def triangular_seq(num)
+  (1..num).each do |i|
+    seq = (i * (i + 1)) / 2
+    if (seq == num)
+      return true
+    end
+  end
 
+  false
+end
+
+def triangular_word?(word)
+  alphabet = ('a'..'z').to_a
+  index_sum = 0
+
+  word.split('').each do |char|
+    index_sum += alphabet.index(char)+1
+  end
+
+  triangular_seq(index_sum)
 end
 
 def consecutive_collapse(array)
+  index = 0
+  fully_collapsed = false
 
+  while (!fully_collapsed)
+    fully_collapsed = true
+
+    while (index < array.length-1)
+      curr_ele = array[index]
+      next_ele = array[index + 1]
+
+      if (curr_ele + 1 == next_ele || curr_ele - 1 == next_ele)
+        fully_collapsed = false
+        array = array[0...index] + array[index+2..-1]
+        index = 0
+      else
+        index += 1
+      end
+    end
+  end
+
+  array
+end
+
+def next_prime(num, location)
+  count = 0
+  prime = 0
+  while (count < location)
+    num += 1
+    if (is_prime?(num))
+      count += 1
+      prime = num
+    end
+  end
+
+  prime
+end
+
+def previous_prime(num, location)
+  count = 0
+  prime = 0
+  while (count > location)
+    num -= 1
+    return nil if num < 2
+
+    if (is_prime?(num))
+      count -= 1
+      prime = num
+    end
+  end
+
+  prime
 end
 
 def pretentious_primes(array, number)
-
+  array.map do |ele|
+    if (number > 0)
+      next_prime(ele, number)
+    else
+      previous_prime(ele, number)
+    end
+  end
 end
