fe11f2af5979ca8277b44d6b4195487abab9834a
Karson Nichols <nicholsk18@gmail.com>
2020-12-22 07:02:23 -0600

    added 'place mark' method to the game

--- software_engineering_foundations/tic_tac_toe/lib/board.rb
+++ software_engineering_foundations/tic_tac_toe/lib/board.rb
@@ -5,7 +5,7 @@ class Board
   end
 
   def valid?(position)
-    position[0] < 3 && position[1] < 3
+    (position[0] >= 0 && position[0] < 3) && (position[1] >= 0 && position[1] < 3)
   end
 
   def empty?(position)
@@ -13,6 +13,10 @@ class Board
   end
 
   def place_mark(position, mark)
-
+    if !self.valid?(position) || !self.empty?(position)
+      rasie "Invalid Mark"
+    else
+      @grid[position[0]][position[1]] = mark
+    end
   end
 end
--- software_engineering_foundations/tic_tac_toe/spec/board_spec.rb
+++ software_engineering_foundations/tic_tac_toe/spec/board_spec.rb
@@ -68,26 +68,46 @@ describe "Board" do
       end
     end
   end
-  #
-  # describe "#place_mark(position, mark)" do
-  #   it "should accept a position and a mark as an arg" do
-  #     mark = :X
-  #     position = [0, 0]
-  #
-  #     expect { board.place_mark(position, mark) }.to_not raise_error
-  #   end
-  #
-  #   context "" do
-  #     it "" do
-  #       expect
-  #       board.instance_variable_set(:grid, [['_', '_', '_'], ['_', :O, :X], ['_', :O, :X]])
-  #     end
-  #   end
-  #
-  #   context "" do
-  #     it "" do
-  #       board.instance_variable_set(:grid, [['_', '_', '_'], ['_', :O, :X], ['_', :O, :X]])
-  #     end
-  #   end
-  # end
+
+  describe "#place_mark(position, mark)" do
+    it "should accept a position and a mark as an arg" do
+      mark = :X
+      position = [0, 0]
+
+      expect { board.place_mark(position, mark) }.to_not raise_error
+    end
+
+    context "when position is valid and position is empty" do
+      it "should set new mark" do
+        board.instance_variable_set(:@grid, [['_', '_', '_'], ['_', :O, :X], ['_', :O, :X]])
+        mark_x = :X
+        mark_o = :O
+        position_1 = [0, 0]
+        position_2 = [0, 1]
+
+        board.place_mark(position_1, mark_x)
+        expect(board.instance_variable_get(:@grid)).to eq([[:X, '_', '_'], ['_', :O, :X], ['_', :O, :X]])
+
+        board.place_mark(position_2, mark_o)
+        expect(board.instance_variable_get(:@grid)).to eq([[:X, :O, '_'], ['_', :O, :X], ['_', :O, :X]])
+      end
+    end
+
+    context "When the position is not valid or position is not empty" do
+      it "should raise error" do
+        board.instance_variable_set(:@grid, [['_', '_', '_'], ['_', :O, :X], ['_', :O, :X]])
+        mark_x = :X
+        mark_o = :O
+
+        position_1 = [3, 0]
+        expect { board.place_mark(position_1, mark_x) }.to raise_error
+
+        position_2 = [1, 1]
+        expect { board.place_mark(position_2, mark_o) }.to raise_error
+
+        position_3 = [-1, 2]
+        expect { board.place_mark(position_3, mark_x) }.to raise_error
+      end
+    end
+  end
 end
