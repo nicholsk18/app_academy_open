8d6f2205e030b1d9c0169a3ec5323c054d88deac
Karson Nichols <nicholsk18@gmail.com>
2021-02-22 08:59:45 -0600

    finished todo board

--- software_engineering_foundations/Todo_Board/todo_board.rb
+++ software_engineering_foundations/Todo_Board/todo_board.rb
@@ -1,39 +1,45 @@
 require_relative 'list.rb'
 
 class TodoBoard
-  def initialize(label)
-    @list = List.new(label)
+  def initialize
+    @lists = {}
   end
 
   def get_command
     print "\nEnter a command: "
-    cmd, *args = gets.chomp.split(' ')
+    cmd, list,  *args = gets.chomp.split(' ')
 
     case cmd
+    when "mklist"
+      @lists[list] = List.new(list)
+    when "ls"
+      @lists.keys.each { |list_label| puts ' ' + list_label }
+    when "showall"
+      @lists.each_value(&:print)
     when 'mktodo'
       # <title>, <deadline>, <optional description>
-      @list.add_item(*args)
+      @lists[list].add_item(*args)
     when "up"
-      @list.up(*args.map(&:to_i))
+      @lists[list].up(*args.map(&:to_i))
     when "down"
-      @list.down(*args.map(&:to_i))
+      @lists[list].down(*args.map(&:to_i))
     when "swap"
-      @list.swap(*args.map(&:to_i))
+      @lists[list].swap(*args.map(&:to_i))
     when "sort"
-      @list.sort_by_date!
+      @lists[list].sort_by_date!
     when "toggle"
-      @list.toggle_item(args[0].to_i)
+      @lists[list].toggle_item(args[0].to_i)
     when "priority"
-      @list.priority
+      @lists[list].priority
     when "rm"
-      @list.remove_item(args[0].to_i)
+      @lists[list].remove_item(args[0].to_i)
     when "purge"
-      @list.purge
+      @lists[list].purge
     when 'print'
       if args.empty?
-        @list.print
+        @lists[list].print
       else
-        @list.print_full_item(args[0].to_i)
+        @lists[list].print_full_item(args[0].to_i)
       end
     when 'quit'
       return false
@@ -50,3 +56,5 @@ class TodoBoard
     end
   end
 end
+
+TodoBoard.new.run
